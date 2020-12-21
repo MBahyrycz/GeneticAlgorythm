e942bd76747ba2f3977e96448df28511984b59c0
NieTrawisz <49477388+NieTrawisz@users.noreply.github.com>
2020-12-21 23:35:44 +0100

    nowe pomiary czasu

--- main.py
+++ main.py
@@ -42,31 +42,92 @@ if __name__ == "__main__":
 
     bestIndividualsPlt.Plot(2)
 
-        # solveTime=list(halfPop[:])
-        # goalFunctions=list(halfPop[:])
         
-        # for index,hp in enumerate(halfPop):
-        #     solveTime[index]=0
-        #     goalFunctions[index]=0
-        #     for i in range(repeats):
-        #         alg = GeneticAlgorythm(hp, 24, 4, popular,bannedAreas=[(1,4),(17,20)],iterationCount=100)
-        #         start=time.time()
-        #         alg.Solve(method)
-        #         solveTime[index]+=time.time()-start
-        #         goalFunctions[index]+=alg.m_AlfaMale.m_Quality
-        #     solveTime[index]/=repeats
-        #     goalFunctions[index]/=repeats
-            
-        # plt.TimePlot(solveTime,2*np.array(halfPop),'Populacja',method)
-        # plt.GoalFunctionOfValues(goalFunctions,2*np.array(halfPop),'Populacja',method)
-
-        
+    
+    
+    
     iterCount=100
     repeats=10
     popular = [1.0, 2.0, 2.14, 3.6, 11.23, 23.51, 17.2, 12.3, 19.2, 7.7]
     popular.sort()
     
-    # #dla rurznych mutate 
+    # dla rurznych banned
+    pltBannedTime=Plotter()
+    bannedCount=list(range(2,30))
+    for method in methods:
+        print()
+        print('Metoda : ',method)
+        print()
+        
+        solveTime=bannedCount[:]
+        
+        for index,ban in enumerate(bannedCount):
+            solveTime[index]=0
+            banlist=np.linspace(0.0,24.0,num=2*ban)
+            bannedList=[]
+            for i in range(ban):
+                bannedList.append((banlist[2*i],banlist[2*i+1]))
+            for i in range(repeats):
+                alg = GeneticAlgorythm(20, 24, 4, popular,bannedAreas=bannedList,iterationCount=iterCount)
+                start=time.time()
+                alg.Solve(method)
+                solveTime[index]+=time.time()-start
+            solveTime[index]/=repeats
+        pltBannedTime.AddValues(bannedCount,solveTime,method,'Liczba miejsc zabronionych','Czas obliczeń')
+    pltBannedTime.Plot(2)
+    
+    # dla rurznych popular
+    pltPopTime=Plotter()
+    popularCount=list(range(2,25))
+    for method in methods:
+        print()
+        print('Metoda : ',method)
+        print()
+        
+        solveTime=popularCount[:]
+        
+        for index,popc in enumerate(popularCount):
+            solveTime[index]=0
+            popularlist=np.logspace(0.0,24.0,num=popc)
+            for i in range(repeats):
+                alg = GeneticAlgorythm(20, 24, 4, popularlist,bannedAreas=[(1,4),(17,20)],iterationCount=iterCount)
+                start=time.time()
+                alg.Solve(method)
+                solveTime[index]+=time.time()-start
+            solveTime[index]/=repeats
+        pltPopTime.AddValues(popularCount,solveTime,method,'Liczba miejsc popularnych','Czas obliczeń')
+    pltPopTime.Plot(2)
+    
+    # dla rurznych stopCount 
+    pltStopsTime=Plotter()
+    pltStopsGF=Plotter()
+    stopCounts=list(range(3,30))
+    for method in methods:
+        print()
+        print('Metoda : ',method)
+        print()
+        
+        solveTime=stopCounts[:]
+        goalFunctions=stopCounts[:]
+        
+        for index,stops in enumerate(stopCounts):
+            solveTime[index]=0
+            goalFunctions[index]=0
+            for i in range(repeats):
+                alg = GeneticAlgorythm(20, 24, stops, popular,bannedAreas=[(1,4),(17,20)],iterationCount=iterCount,minStopsDistance=5/stops,maxStopsDistance=40/stops)
+                start=time.time()
+                alg.Solve(method)
+                solveTime[index]+=time.time()-start
+                goalFunctions[index]+=alg.m_AlfaMale.m_Quality
+            solveTime[index]/=repeats
+            goalFunctions[index]/=repeats
+        #print(goalFunctions)
+        pltStopsTime.AddValues(stopCounts,solveTime,method,'Liczba przystanków','Czas obliczeń')
+        pltStopsGF.AddValues(stopCounts,goalFunctions,method,'Liczba przystanków','Funkcja celu')
+    pltStopsTime.AddPlots(pltStopsGF.GetTuples())
+    pltStopsTime.Plot(4)
+    
+    # dla rurznych mutate 
     pltMutateTime=Plotter()
     pltMutateGF=Plotter()
     mutate=(np.linspace(0.0,1.0,num=20)).tolist()
@@ -82,7 +143,7 @@ if __name__ == "__main__":
             solveTime[index]=0
             goalFunctions[index]=0
             for i in range(repeats):
-                alg = GeneticAlgorythm(20, 24, 4, popular,bannedAreas=[(1,4),(17,20)],iterationCount=100,mutationProbability=m)
+                alg = GeneticAlgorythm(20, 24, 4, popular,bannedAreas=[(1,4),(17,20)],iterationCount=iterCount,mutationProbability=m)
                 start=time.time()
                 alg.Solve(method)
                 solveTime[index]+=time.time()-start
@@ -94,8 +155,6 @@ if __name__ == "__main__":
         pltMutateGF.AddValues(mutate,goalFunctions,method,'Prawdopodobieństwo mutacji','Funkcja celu')
     pltMutateTime.AddPlots(pltMutateGF.GetTuples())
     pltMutateTime.Plot(4)
-    # pltMutateTime.Plot(2)
-    # pltMutateGF.Plot(2)
     
     # dla rurznych populationCount
     halfPop=range(1,30)
@@ -113,7 +172,7 @@ if __name__ == "__main__":
             solveTime[index]=0
             goalFunctions[index]=0
             for i in range(repeats):
-                alg = GeneticAlgorythm(hp, 24, 4, popular,bannedAreas=[(1,4),(17,20)],iterationCount=100)
+                alg = GeneticAlgorythm(hp, 24, 4, popular,bannedAreas=[(1,4),(17,20)],iterationCount=iterCount)
                 start=time.time()
                 alg.Solve(method)
                 solveTime[index]+=time.time()-start
@@ -125,7 +184,4 @@ if __name__ == "__main__":
         pltPopGF.AddValues(2*np.array(halfPop),goalFunctions,method,'Populacja','Funkcja celu')
     
     pltPopTime.AddPlots(pltPopGF.GetTuples())
-    # pltPopTime.Plot(2)
-    # pltPopGF.Plot(2)
     pltPopTime.Plot(4)
-    
